078e12f491c3c05380216a7b0f240a41ab3fefef
Karuppasamych <106671462+Karuppasamych@users.noreply.github.com>
2022-06-08 13:46:42 +0530

    Add files via upload

---
+++ Function_Using_Arithmatic_operations.js
@@ -0,0 +1,36 @@
+
+// Function using arithmatic operations
+let num1 = prompt('Enter the number');
+let num2 = prompt('Enter the number');
+let opr = prompt('Enter the operands');
+
+function arithmatic(num1, num2, opr){
+
+        if(opr == '+'){
+
+            let total = Number(num1) + Number(num2)
+            console.log('Sum value = ',total)
+            return total;
+        }
+
+        else if(opr == '-'){
+            let total = Number(num1) - Number(num2)
+            console.log('Substraction value = ',total)
+            return total;
+        }
+
+        else if(opr == '*'){
+            let total = Number(num1) * Number(num2)
+            console.log('Multiplication value = ',total)
+            return total;
+        }
+
+        else if(opr == '/'){
+            let total = Number(num1) / Number(num2)
+            console.log('Division value = ',total)
+            return total;
+        }
+}
+
+var result = arithmatic(num1 ,num2 , opr)
+console.log(result);
